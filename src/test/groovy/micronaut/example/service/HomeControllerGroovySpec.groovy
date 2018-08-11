a40c8ae6a89d6b6b0e14dd5f005bde7a3ac3af5e
coestre <christian.oestreich@optum.com>
2018-08-11 10:32:28 -0700

    adding more examples that do not work

---
+++ src/test/groovy/micronaut/example/service/HomeControllerGroovySpec.groovy
@@ -0,0 +1,32 @@
+package micronaut.example.service
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class HomeControllerGroovySpec extends Specification {
+
+    @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+    @Shared @AutoCleanup RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+
+
+    void "test index"() {
+        given:
+        HttpResponse response = client.toBlocking().exchange("/home")
+
+        expect:
+        response.status == HttpStatus.OK
+    }
+
+
+
+    void "should fail"() {
+        expect:
+        false
+    }
+}
