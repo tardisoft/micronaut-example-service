759f90d11cae5d357715d800796ebd97ceea6a2c
coestre <christian.oestreich@optum.com>
2018-08-11 08:15:03 -0700

    Demo Broken Maven Project

---
+++ src/main/java/micronaut/example/service/IndexController.java
@@ -0,0 +1,15 @@
+package micronaut.example.service;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.reactivex.Single;
+
+@Controller("/")
+public class IndexController {
+
+    @Get()
+    public Single<String> index() {
+        return Single.just("Use /{transactionId}");
+    }
+
+}
